07eb1d78857da0e3dc34a19d5ed895b3e3b33e8e
NaifatSelina <naifatselina@icloud.com>
2022-05-31 00:08:40 +0000

    Add pointer cursors to buttons

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,15 +1,1 @@
-const startButton = document.getElementById('button-start')
 
-startButton.addEventListener('click', startGame)
-
-function startGame() {
-    console.log('Started')
-}
-
-function goNextQuestion() {
-
-}
-
-function selectAnswer() {
-
-}
