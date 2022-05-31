3350734ff9db2dd32cbfe4df02a18b8a40955371
NaifatSelina <naifatselina@icloud.com>
2022-05-31 00:46:06 +0000

    Add questions to javascript with placeholder text

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,1 +1,89 @@
+const question = document.getElementById('#question');
+const answers = document.getElementsByClassName('.question-btns');
 
+let currentQuestion = {};
+let acceptingAnswers = true;
+let availableQuestions = [];
+
+let questions = [
+    {
+        question: 'What is Spider-Mans real name?', 
+        answer1: 'Peter Parker', 
+        answer2: 'Harry Osbourn',
+        answer3: 'Steven Strange',
+        answer4: 'Steve Rogers',
+        answer: 1,
+    },
+    {
+        question: 'What is Spider-Mans real name?', 
+        answer1: 'Peter Parker', 
+        answer2: 'Harry Osbourn',
+        answer3: 'Steven Strange',
+        answer4: 'Steve Rogers',
+        answer: 1,
+    },
+    {
+        question: 'What is Spider-Mans real name?', 
+        answer1: 'Peter Parker', 
+        answer2: 'Harry Osbourn',
+        answer3: 'Steven Strange',
+        answer4: 'Steve Rogers',
+        answer: 1,
+    },
+    {
+        question: 'What is Spider-Mans real name?', 
+        answer1: 'Peter Parker', 
+        answer2: 'Harry Osbourn',
+        answer3: 'Steven Strange',
+        answer4: 'Steve Rogers',
+        answer: 1,
+    },
+    {
+        question: 'What is Spider-Mans real name?', 
+        answer1: 'Peter Parker', 
+        answer2: 'Harry Osbourn',
+        answer3: 'Steven Strange',
+        answer4: 'Steve Rogers',
+        answer: 1,
+    },
+    {
+        question: 'What is Spider-Mans real name?', 
+        answer1: 'Peter Parker', 
+        answer2: 'Harry Osbourn',
+        answer3: 'Steven Strange',
+        answer4: 'Steve Rogers',
+        answer: 1,
+    },
+    {
+        question: 'What is Spider-Mans real name?', 
+        answer1: 'Peter Parker', 
+        answer2: 'Harry Osbourn',
+        answer3: 'Steven Strange',
+        answer4: 'Steve Rogers',
+        answer: 1,
+    },
+    {
+        question: 'What is Spider-Mans real name?', 
+        answer1: 'Peter Parker', 
+        answer2: 'Harry Osbourn',
+        answer3: 'Steven Strange',
+        answer4: 'Steve Rogers',
+        answer: 1,
+    },
+    {
+        question: 'What is Spider-Mans real name?', 
+        answer1: 'Peter Parker', 
+        answer2: 'Harry Osbourn',
+        answer3: 'Steven Strange',
+        answer4: 'Steve Rogers',
+        answer: 1,
+    },
+    {
+        question: 'What is Spider-Mans real name?', 
+        answer1: 'Peter Parker', 
+        answer2: 'Harry Osbourn',
+        answer3: 'Steven Strange',
+        answer4: 'Steve Rogers',
+        answer: 1,
+    },
+];
